557e7440cf6de3ba8c806cb80e6c0f499b1e09cc
Davi Rodrigues <davirodriguescarvalhos@gmail.com>
2024-11-02 19:30:08 -0300

    cors

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const app = express();
 const taskRoutes = require('./routes/taskRoutes');
 const path = require('path');  // Importa o módulo `path`
 const cors = require('cors');
-app.use(cors());
+app.use(cors({
+    origin: 'https://lista-de-tarefas-ufce.onrender.com' // Substitua pela URL do seu frontend no Render
+}));
 app.use(express.json());
 app.use('/api', taskRoutes);
 
