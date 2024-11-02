19b3302f8d2458b4fc0d18b5bd3e693ac3117c32
Davi Rodrigues <davirodriguescarvalhos@gmail.com>
2024-11-02 20:37:42 -0300

    odoso

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
-// app.js
 const express = require('express');
+const path = require('path');
 const app = express();
 const taskRoutes = require('./routes/taskRoutes');
-const path = require('path');  // Importa o módulo `path`
 const cors = require('cors');
-app.use(cors());
-app.use(express.json());
-app.use('/api', taskRoutes);
 
+app.use(cors({
+  origin: 'https://lista-de-tarefas-ufce.onrender.com' // Substitua pela URL do seu frontend no Render
+}));
+
+// Middleware para servir arquivos estáticos da pasta "public"
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Rota para a API
+app.use('/api', taskRoutes);
+
+// Rota para servir o "index.html" para qualquer rota não especificada (SPA)
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}`);
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
